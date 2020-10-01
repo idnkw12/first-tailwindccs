3588908bf2767b5b0510f110dd9089957771fbba
wsngiri <prolink714@gmail.com>
2020-10-01 19:40:18 +0700

    add some

---
+++ public/app.js
@@ -0,0 +1,7 @@
+const burger = document.querySelector('#burger');
+const menu = document.querySelector('#menu');
+
+burger.addEventListener('click', () => {
+    menu.classList.contains('hidden') ?
+        menu.classList.remove('hidden') : menu.classList.add('hidden')
+})
